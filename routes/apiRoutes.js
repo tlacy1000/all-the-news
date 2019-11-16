9cd614264e953217d5bfcbe4d0447b71da012fa7
tlacy1000 <tlacy1000@gmail.com>
2019-11-16 14:51:43 -0600

    changed mongo uri

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -41,90 +41,90 @@ module.exports = function(app) {
         }
       });
       // Send a message to the client
-      // res.redirect("/");
+      res.redirect("/");
     });
   });
 
-  // app.get("/api/articles", function(req, res) {
-  //   db.Article.find({})
-  //     .populate("note")
-  //     .then(function(data) {
-  //       res.json(data);
-  //     });
-  // });
+  app.get("/api/articles", function(req, res) {
+    db.Article.find({})
+      .populate("note")
+      .then(function(data) {
+        res.json(data);
+      });
+  });
 
   // // put route to updated the article to be saved:true
-  // app.post("/api/saved/:id", function(req, res) {
-  //   // res.redirect("/")
-  //   db.Article.updateOne(
-  //     { _id: req.params.id },
-  //     { $set: { saved: true } },
-  //     function(err, doc) {
-  //       if (err) {
-  //         res.send(err);
-  //       } else {
-  //         // res.redirect("/");
-  //       }
-  //     }
-  //   );
-  // });
+  app.post("/api/saved/:id", function(req, res) {
+    // res.redirect("/")
+    db.Article.updateOne(
+      { _id: req.params.id },
+      { $set: { saved: true } },
+      function(err, doc) {
+        if (err) {
+          res.send(err);
+        } else {
+          // res.redirect("/");
+        }
+      }
+    );
+  });
 
-  // Save a note route
-  // app.post("/api/saved/notes/:id", function(req, res) {
-  //   db.Note.create(req.body)
-  //     .then(function(dbNote) {
-  //       console.log(req.params.id, dbNote);
-  //       //update the Article and push the note id into the array
-  //       return db.Article.findOneAndUpdate(
-  //         { _id: req.params.id },
-  //         { $push: { note: dbNote._id } }
-  //       );
-  //     })
-  //     .then(function(dbArticle) {
-  //       res.json(dbArticle);
-  //     })
-  //     .catch(function(err) {
-  //       res.json(err);
-  //     });
-  // });
+  Save a note route
+  app.post("/api/saved/notes/:id", function(req, res) {
+    db.Note.create(req.body)
+      .then(function(dbNote) {
+        console.log(req.params.id, dbNote);
+        //update the Article and push the note id into the array
+        return db.Article.findOneAndUpdate(
+          { _id: req.params.id },
+          { $push: { note: dbNote._id } }
+        );
+      })
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
 
   // // Delete a note route
-  // app.delete("/api/notes/:id", function(req, res) {
-  //   console.log(req.params.id);
-  //   db.Note.findOneAndDelete({ _id: req.params.id })
-  //     .then(function(dbNote) {
-  //       return db.Article.findOneAndUpdate(
-  //         { note: req.params.id },
-  //         { $pullAll: [{ note: req.params.id }] }
-  //       );
-  //     })
-  //     .then(function(dbArticle) {
-  //       res.json(dbArticle);
-  //     })
-  //     .catch(function(err) {
-  //       res.json(err);
-  //     });
-  // });
+  app.delete("/api/notes/:id", function(req, res) {
+    console.log(req.params.id);
+    db.Note.findOneAndDelete({ _id: req.params.id })
+      .then(function(dbNote) {
+        return db.Article.findOneAndUpdate(
+          { note: req.params.id },
+          { $pullAll: [{ note: req.params.id }] }
+        );
+      })
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
 
   //Delete Saved Article Route
-  // app.delete("/api/articles/:id", function(req, res) {
-  //   db.Article.findOneAndDelete({ _id: req.params.id })
-  //     .then(function(dbArticle) {
-  //       res.json(dbArticle);
-  //     })
-  //     .catch(function(err) {
-  //       res.json(err);
-  //     });
-  // });
+  app.delete("/api/articles/:id", function(req, res) {
+    db.Article.findOneAndDelete({ _id: req.params.id })
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
 
   // // Clear all Unsaved Articles
-  // app.delete("/api/articles", function (req, res) {
-  //   db.Article.remove({saved: false})
-  //     .then(function (dbArticle) {
-  //       res.json(dbArticle);
-  //     })
-  //     .catch(function (err) {
-  //       res.json(err);
-  //     });
-  // });
+  app.delete("/api/articles", function (req, res) {
+    db.Article.remove({saved: false})
+      .then(function (dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function (err) {
+        res.json(err);
+      });
+  });
 };
