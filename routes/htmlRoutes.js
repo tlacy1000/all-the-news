7765093a1982cba77e532e024c225028d150d106
tlacy1000 <tlacy1000@gmail.com>
2019-11-16 14:44:38 -0600

    commenting out db calls 2

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,30 +2,30 @@ var db = require("../models");
 
 module.exports = function (app) {
     // Load index page
-    app.get("/", function (req, res) {
-        db.Article.find({ "saved": false }, function (error, data) {
-            var hbsObject = {
-                article: data
-            };
-            // console.log(hbsObject);
-            res.render("index", hbsObject);
-        });
-    });
+    // app.get("/", function (req, res) {
+    //     db.Article.find({ "saved": false }, function (error, data) {
+    //         var hbsObject = {
+    //             article: data
+    //         };
+    //         // console.log(hbsObject);
+    //         res.render("index", hbsObject);
+    //     });
+    // });
 
-    app.get("/saved", function (req, res) {
-        db.Article.find({ saved: true }).populate("note").exec(function(error, data) {
-          var hbsObject = {
-            article: data
-          };
+    // app.get("/saved", function (req, res) {
+    //     db.Article.find({ saved: true }).populate("note").exec(function(error, data) {
+    //       var hbsObject = {
+    //         article: data
+    //       };
           
-          if(error){
-              res.send(error)
-          } else {
-              res.render("saved", hbsObject);
-          }
+    //       if(error){
+    //           res.send(error)
+    //       } else {
+    //           res.render("saved", hbsObject);
+    //       }
           
-        });
-    });
+    //     });
+    // });
 
     
     // Render 404 page for any unmatched routes
