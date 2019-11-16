eb2e9c306252d59e4a4b454bdde742ddc8493482
tlacy1000 <tlacy1000@gmail.com>
2019-11-16 15:37:03 -0600

    resetting defaults

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,15 +2,15 @@ var db = require("../models");
 
 module.exports = function (app) {
     // Load index page
-    // app.get("/", function (req, res) {
-    //     db.Article.find({ "saved": false }, function (error, data) {
-    //         var hbsObject = {
-    //             article: data
-    //         };
-    //         // console.log(hbsObject);
-    //         res.render("index", hbsObject);
-    //     });
-    // });
+    app.get("/", function (req, res) {
+        db.Article.find({ "saved": false }, function (error, data) {
+            var hbsObject = {
+                article: data
+            };
+            // console.log(hbsObject);
+            res.render("index", hbsObject);
+        });
+    });
 
     app.get("/saved", function (req, res) {
         db.Article.find({ saved: true }).populate("note").exec(function(error, data) {
