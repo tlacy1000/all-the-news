be05718a18c393841417d62f7ec6670cd68b2ce6
tlacy1000 <tlacy1000@gmail.com>
2019-11-16 15:14:17 -0600

    fixed mongo_uri

--- server.js
+++ server.js
@@ -23,9 +23,8 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // connect to mongodb
-var MONGODB_URI =   "mongodb://all-the-news123.herokuapp.com:27017" || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI =   process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-// process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 
